d773f565d1d874d61eba89bc272757560a112855
Anja-Klassen <anja.klassen@posteo.de>
2024-09-26 11:46:03 +0200

    add solution

--- js-functions_greeting-page/js/index.js
+++ js-functions_greeting-page/js/index.js
@@ -23,12 +23,34 @@ Change the contents of this page depending on the current day and time.
 const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
-  // Code here
+  const currentTime = new Date().getHours();
+  if (currentTime >=6 && currentTime<=12) {
+  return "Good Morning";
+  }
+  else if (currentTime>=13 && currentTime<=18) {
+    return "Good Afternoon"
+  }
+  else if (currentTime>=19 && currentTime<=20) {
+    return "Good Evening"
+  }
+  else {
+    return "Good Evening"
+  }
 }
 
+//Alternative: drei mal if und dann nur return
+console.log(new Date().getDay());
 function getDayColor() {
-  // Code here
+  const currentDay = 1;
+  if(currentDay===1) {
+    return document.body.style.backgroundColor = "darkgray";
 }
-
+  if(currentDay>=2 && currentDay<=5) {
+    return document.body.style.backgroundColor = "lightblue";
+  }
+  else {
+    return document.body.style.backgroundColor = "hotpink";
+  }
+}
 display.textContent = getGreeting();
 document.body.style.backgroundColor = getDayColor();
